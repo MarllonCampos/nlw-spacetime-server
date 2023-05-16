75fbe22a3ebafefce5bc27ff8d3fb1dc1d69877e
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-05-16 01:32:52 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+const prisma = new PrismaClient()
+app.get('/', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server Liisten on Port: 3333 ')
+})
